9153daa13538c3cca35b63df7379e4f128cab48c
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2021-02-08 21:07:39 +0300

    did

---
+++ 28/phone.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+<h2>Please, enter your phone number<br>
+    and we will call you back if you are from Vitebsk</h2>
+<form action="?" method="post">
+    Phone: <input type="text" name="phone">
+    <input type="submit" value="ok">
+</form>
+<br>
+<?php
+$phone = $_POST["phone"];
+if (preg_match("/^((80212|\+375(\-)?(\()?212(\))?)?[0-9,\-]{6,9})/iu", $phone)) {
+    echo "Thanks, we will call you back to " . $phone;
+} else {
+    echo "Sorry, you are not from Vitebsk";
+}
+?>
+</body>
+
+</html>
