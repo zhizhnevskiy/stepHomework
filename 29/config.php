511758dcc7f5303d1f60ab988b007e54084d57a3
Zhiznevskiy_Yura <zhizhnevskiy@yandex.ru>
2021-02-12 21:22:42 +0300

    did

--- 29/config.php
+++ 29/config.php
@@ -1,51 +1,54 @@
 <?php
+$ok = true;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    if (empty($_POST["name"])) {
-        $nameErr = "Name is required";
-    } else {
-        $name = test_input($_POST["name"]);
-        if (!preg_match("/^[a-zа-я-' ]*$/iu", $name)) {
-            $nameErr = "Only letters and white space allowed";
-        }
+if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+} else {
+    $name = test_input($_POST["name"]);
+    if (!preg_match("/^[a-zа-я-' ]*$/iu", $name)) {
+        $nameErr = "Only letters and white space allowed";
+        $ok = false;
     }
+}
 
-    if (empty($_POST["surname"])) {
-        $surnameErr = "Surname is required";
-    } else {
-        $surname = test_input($_POST["surname"]);
-        if (!preg_match("/^[a-zа-я-' ]*$/iu", $surname)) {
-            $surnameErr = "Only letters and white space allowed";
-        }
+if (empty($_POST["surname"])) {
+    $surnameErr = "Surname is required";
+} else {
+    $surname = test_input($_POST["surname"]);
+    if (!preg_match("/^[a-zа-я-' ]*$/iu", $surname)) {
+        $surnameErr = "Only letters and white space allowed";
+        $ok = false;
     }
+}
 
-    if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-    } else {
-        $email = test_input($_POST["email"]);
-        if (!preg_match("/^[0-9A-Z_]{5,}@[0-9A-Z]{2,}\.[A-Z]{2,6}$/iu", $email)) {
-            $emailErr = "Invalid email format";
-        }
+if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+} else {
+    $email = test_input($_POST["email"]);
+    if (!preg_match("/^[0-9A-Z_]{5,}@[0-9A-Z]{2,}\.[A-Z]{2,6}$/iu", $email)) {
+        $emailErr = "Invalid email format";
+        $ok = false;
     }
+}
 
-    if (empty($_POST["phone"])) {
-        $phoneErr = "Phone is required";
-    } else {
-        $phone = test_input($_POST["phone"]);
-        $phone = preg_replace("/[^0-9\+]/","", $phone);
-        if (!preg_match("/^(80|\+375)(29|33|44|212)\d{6,7}$/iu", $phone)) {
-            $phoneErr = "Invalid phone format";
-        }
+if (empty($_POST["phone"])) {
+    $phoneErr = "Phone is required";
+} else {
+    $phone = test_input($_POST["phone"]);
+    $phone = preg_replace("/[^0-9\+]/", "", $phone);
+    if (!preg_match("/^(80|\+375)(29|33|44|212)\d{6,7}$/iu", $phone)) {
+        $phoneErr = "Invalid phone format";
+        $ok = false;
     }
+}
 
-    if (empty($_POST["social"])) {
-        $socialErr = "Social network is required";
-    } else {
-        $social = test_input($_POST["social"]);
-        if (!preg_match("/((?:https?|ftp):\/\/|www)\.[-a-z0-9+&@#\/%?=~_|!:,.;]*/iu", $social)) {
-            $socialErr = "Invalid social network link";
-        }
+if (empty($_POST["social"])) {
+    $socialErr = "Social network is required";
+} else {
+    $social = test_input($_POST["social"]);
+    if (!preg_match("/((?:https?|ftp):\/\/|www)\.[-a-z0-9+&@#\/%?=~_|!:,.;]*/iu", $social)) {
+        $socialErr = "Invalid social network link";
+        $ok = false;
     }
 }
 
@@ -57,11 +60,9 @@ function test_input($data)
     return $data;
 }
 
-if (!empty($name) & !empty($surname) & !empty($email) & !empty($phone) & !empty($social)) {
-    $row = "\n<----->\n" . $name . "\n" .  $surname . "\n" .$email .
+if ($ok) {
+    $row = "\n<----->\n" . $name . "\n" . $surname . "\n" . $email .
         "\n" . $phone . "\n" . $social .
         "\n" . date("d.m.Y") . "\n" . date("H:i");
     file_put_contents("contact.txt", $row, FILE_APPEND);
 }
-// header("Location: ?");
-// die();
--- 29/contact.php
+++ 29/contact.php
@@ -1,6 +1,15 @@
 <?php
-$nameErr = $surnameErr = $emailErr = $phoneErr = $socialErr = "";
-$name = $surname = $email = $phone = $social = "";
+/** @var string $name
+ * @var string $nameErr
+ * @var string $surname
+ * @var string $surnameErr
+ * @var string $email
+ * @var string $emailErr
+ * @var string $phone
+ * @var string $phoneErr
+ * @var string $social
+ * @var string $socialErr
+ */
 require_once "config.php";
 
 ?>
@@ -18,20 +27,20 @@ require_once "config.php";
 <div class="box">
     <h2>Contact information</h2>
     <form method="POST" action="?">
-        <b>Name:</b> <input type="text" name="name" value="<?php echo $name; ?>">
-        <span class="error">* <?php echo $nameErr; ?></span>
+        <b>Name:</b> <input type="text" name="name" value="<?= $name ?>">
+        <span class="error">* <?= $nameErr ?></span>
         <br><br>
-        <b>Surname:</b> <input type="text" name="surname" value="<?php echo $surname; ?>">
-        <span class="error">* <?php echo $surnameErr; ?></span>
+        <b>Surname:</b> <input type="text" name="surname" value="<?= $surname ?>">
+        <span class="error">* <?= $surnameErr ?></span>
         <br><br>
-        <b>E-mail:</b> <input type="text" name="email" value="<?php echo $email; ?>">
-        <span class="error">* <?php echo $emailErr; ?></span>
+        <b>E-mail:</b> <input type="text" name="email" value="<?= $email ?>">
+        <span class="error">* <?= $emailErr ?></span>
         <br><br>
-        <b>Phone:</b> <input type="text" name="phone" value="<?php echo $phone; ?>">
-        <span class="error">* <?php echo $phoneErr; ?></span>
+        <b>Phone:</b> <input type="text" name="phone" value="<?= $phone ?>">
+        <span class="error">* <?= $phoneErr ?></span>
         <br><br>
-        <b>Social network:</b> <input type="text" name="social" value="<?php echo $social; ?>">
-        <span class="error">* <?php echo $socialErr; ?></span>
+        <b>Social network:</b> <input type="text" name="social" value="<?= $social ?>">
+        <span class="error">* <?= $socialErr ?></span>
         <br><br>
         <input type="submit" name="submit" value="Submit">
         <br><br>
